8b8c05e6971d55d257051f976cefb28300ffe823
FlaminiaM <flaminia.mazzitelli.92@gmail.com>
2024-01-11 11:32:16 +0000

    added config file

--- src/api/movies.js
+++ src/api/movies.js
@@ -1,5 +1,7 @@
 import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react';
 
+import { api_key } from '../config';
+
 export const moviesApi = createApi({
     reducerPath: 'api',
     baseQuery: fetchBaseQuery({
@@ -7,7 +9,7 @@ export const moviesApi = createApi({
         prepareHeaders: (headers, { getState }) => {
             headers.set(
                 'Authorization',
-                `Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiI3NjAwYzFjYTRlYWI0MzRkZjdmNWRmYTY5M2ZjYWQyMyIsInN1YiI6IjY1OTdlOTQ5NzI2ZmIxMDIwZWFiMjhiMiIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.snbME917q4u2bNVw7w0oHdnyu_KzRW4Y0fdX9lZ-ALk`
+                api_key
             );
             return headers;
         },
