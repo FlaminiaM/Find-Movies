06b9d3e962a0f487929132777d89d4c4c973bd5f
Flaminia Mazzitelli <flaminiamazzitelli@Flaminias-MacBook-Pro.local>
2024-01-17 16:10:27 +0000

    added credit

--- src/App.js
+++ src/App.js
@@ -7,6 +7,8 @@ import Homepage from './components/pages/Homepage/Homepage';
 import DetailsPage from './components/pages/DetailsPage/DetailsPage';
 import 'react-loading-skeleton/dist/skeleton.css';
 
+import tmdbLogo from './assets/images/tmdb.svg'
+
 function App() {
   return (
     <div className="App">
@@ -16,6 +18,7 @@ function App() {
         <Route path="/movies/:id" element={<DetailsPage type='movie' />} />
         <Route path="/series/:id" element={<DetailsPage type='tv' />}  />
       </Routes>
+      <div className='footer'><img src={tmdbLogo} alt='tmdb logo'/> This product uses the TMDB API but is not endorsed or certified by TMDB</div>
     </div>
   )
 }
